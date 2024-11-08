adf91d6bc8d7fd115dd66834129eec55e650aabd
DanielFerraiuolo98 <daniel.ferraiuolo.98@gmail.com>
2024-11-08 17:04:09 +0100

    prezzi dei biglietti fatti

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ const  firstNameField = document.getElementById("Name-field");
 const lastNameField = document.getElementById("Lastname-field");
 const kmField = document.getElementById("km-field");
 const etaField = document.getElementById("Eta-field");
+const result = document.getElementById("result");
 const button = document.querySelector("button");
 
 const firstName = document.getElementById("Name");
@@ -18,6 +19,25 @@ button.addEventListener("click", function (event){
     lastName.innerText = lastNameField.value
     km.innerText = kmField.value
     eta.innerText = etaField.value
+    
+    const priceKm = 0.21;
+    const lunghezzaKm = parseInt(kmField.value);
+    const price = priceKm * lunghezzaKm;
+
+    const junior = price * 20 / 100;
+    const senior = price * 40 / 100;
+    
+    if(junior <= etaField.value){
+        result.innerText = (price-junior).toFixed(2);
+    }
+    else if(senior <= etaField.value){
+        result.innerText = (price-senior).toFixed(2);
+        
+    }
+    else{
+        result.innerText = price.toFixed(2);
+    } 
+
 })
    
   
